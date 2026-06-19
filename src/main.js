c8c13fda5a9ba8307c1364e9ae647e728770b8c7
James Morrissey <jammim19@gmail.com>
2026-06-19 17:57:26 +0100

    Fixed up the naming conventions and the stylelint fixes

--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@ import "./js/axe-TestingHandler";
 import qrCodeImage from "./assets/image-qr-code.png";
 
 document.querySelector("#app").innerHTML = `
-<article class="qr_comp__card">
-  <picture class="qr_comp__holder">
-    <img class="qr_comp__image" src="${qrCodeImage}" alt="QR code" srcset="" />
+<article class="qr-code-card">
+  <picture class="qr-code-card__holder">
+    <img class="qr-code-card__image" src="${qrCodeImage}" alt="QR code" srcset="" />
   </picture>
-  <h1 class="qr_comp__heading" >Improve your front-end skills by building projects</h1>
-  <p class="qr_comp__paragraph" >
+  <h1 class="qr-code-card__heading" >Improve your front-end skills by building projects</h1>
+  <p class="qr-code-card__paragraph" >
     Scan the QR code to visit Frontend Mentor and take your coding skills
     to the next level
   </p>
