7a66c31a87ed0060b665368bb17f22b0f05d5f79
James Morrissey <jammim19@gmail.com>
2026-06-19 17:39:13 +0100

    Add an alt text to an image

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import qrCodeImage from "./assets/image-qr-code.png";
 document.querySelector("#app").innerHTML = `
 <article class="qr_comp__card">
   <picture class="qr_comp__holder">
-    <img class="qr_comp__image" src="${qrCodeImage}" alt="" srcset="" />
+    <img class="qr_comp__image" src="${qrCodeImage}" alt="QR code" srcset="" />
   </picture>
   <h1 class="qr_comp__heading" >Improve your front-end skills by building projects</h1>
   <p class="qr_comp__paragraph" >
